6de618fd9e472d8d09861ebba1181a0d16c6494a
gaganbatth <gagandeepbatth145@gmail.com>
2026-07-21 13:38:25 +0530

    Add files via upload

---
+++ Day19/App.jsx
@@ -0,0 +1,23 @@
+import Counter from "./Counter";
+import Toggle from "./Toggle";
+import Conditional from "./Conditional";
+
+function App() {
+  return (
+    <div>
+      <h1>React State & Event Handling</h1>
+
+      <Counter />
+
+      <hr />
+
+      <Toggle />
+
+      <hr />
+
+      <Conditional />
+    </div>
+  );
+}
+
+export default App;
